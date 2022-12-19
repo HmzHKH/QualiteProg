98a05113d2bfbe51fb7e8066b9480d99910a8901
HmzHKH <116167301+HmzHKH@users.noreply.github.com>
2022-12-19 21:11:41 +0100

    rajout de fonctions et maj + correction erreurs
    Fonctions rajoutés:
    Dans AireDeJeu
    -posJoueur
    -estOccupeType
    Dans fauve
    -killFauve
    -PiegeAPic
    et les deux fonctions Deplacement
    dans MainWindowTest:
    -correction des erreurs 
    -fonction AfficheAireDejeu

--- mainWindowTest.cpp
+++ mainWindowTest.cpp
@@ -20,31 +20,53 @@ while (menu)
     
 void jouer()
 {
+    int val;
     std::vector<std::vector<int>> AireDeJeu;
     joueur j;
     std::vector<fauve> fauves;
+    std::vector<piegeAPic> pieges;
     initAireDeJeu(AdJ);
     while (!gameOver && !fauve[i].empty())
     {
-        SaisieDeplacement();
-        j.deplacement(valeur);
+        val=SaisieDeplacement();
+        j.deplacement(val);
         for(int i=0;i<fauves.size();++i)
         {
-            D.changeDeplacement();
-            fauves[i].deplacement(AireDeJeu,D);
+            fauves[i].deplacement(AireDeJeu,j,fauves,pieges);// on deplace les fauves
         }
     }
 
 }
 
-void Saisiedeplacement()
+int Saisiedeplacement()
 {
     int valeur = 0;
     while(valeur<1 || valeur>9)
     {
         cout<<"envoyez la valeur de deplacement: ";
         cin>>valeur;
-    }   
+    }
+    return valeur;   
 }
 
 
+void afficheAiredj(const std::vector<std::vector<int>>& ADJ)//affichage du tableau et conversion en symboles
+{
+    for(int i=0; i<ADJ.size();i++)
+    {
+        for(int j=0;j<ADJ[0].size();j++)
+        {
+            if(ADJ[i][j]==0)
+                cout<<'|'<<" "<<'|';
+            else if(ADJ[i][j]==1)
+                cout<<'|'<<'J'<<'|';
+            else if(ADJ[i][j]==2)
+                cout<<'|'<<'L'<<'|';
+            else if(ADJ[i][j]==3)
+                cout<<'|'<<'T'<<'|';
+            else if(ADJ[i][j]==4)
+                cout<<'|'<<'P'<<'|';                
+        }
+        cout<<endl;
+    }
+}
